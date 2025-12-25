44c0b4c3272afc4466e0e35cc3c8eda093d1d256
Joseph Corneli <jcorneli@brookes.ac.uk>
2025-12-25 21:20:44 +0000

    update test runner

--- dev/run-tests.sh
+++ dev/run-tests.sh
@@ -19,6 +19,7 @@ TEST_FILES=(
   arxana-export-test.el
   arxana-saving-test.el
   arxana-browse-test.el
+  arxana-docbook-order-test.el
   arxana-xtdb-browse-test.el
   arxana-compat-test.el
   arxana-flexiarg-normalize-test.el
@@ -28,7 +29,6 @@ for test_file in "${TEST_FILES[@]}"; do
   echo "==> Running ${test_file}"
   "$EMACS_BIN" --batch \
     -l "$ROOT/dev/bootstrap.el" \
-    --eval "(arxana-build)" \
     -l "$ROOT/test/${test_file}" \
     -f ert-run-tests-batch-and-exit
   echo
